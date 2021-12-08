60ada02b3477337c9cdd3d88d2e08860a882300d
daskza19 <josep.sanchez.arbona@gmail.com>
2021-12-08 11:01:44 +0100

    Instantiate more than 1 card in players

--- Uno Online/Assets/Scripts/ServerManager.cs
+++ Uno Online/Assets/Scripts/ServerManager.cs
@@ -40,9 +40,9 @@ public class ServerManager : MonoBehaviour
     private bool wannaStartTimer = false;
     public int whatToDo = 1000;
 
-    private int numberNewCard;
-    private int randomizerNewCard;
-    private int number2NewCard;
+    private List<int> numberNewCard = new List<int>();
+    private List<int> randomizerNewCard = new List<int>();
+    private List<int> number2NewCard = new List<int>();
     private bool wannaRandomNumbers;
     #endregion
 
@@ -72,9 +72,20 @@ public class ServerManager : MonoBehaviour
     {
         if (wannaRandomNumbers)
         {
-            numberNewCard = UnityEngine.Random.Range(0, 10);
-            randomizerNewCard = UnityEngine.Random.Range(1, 24);
-            number2NewCard = UnityEngine.Random.Range(1, 3);
+            numberNewCard.Clear();
+            randomizerNewCard.Clear();
+            number2NewCard.Clear();
+
+            for (int i = 0; i < 100; i++)
+            {
+                int num1 = UnityEngine.Random.Range(0, 10);
+                int num2 = UnityEngine.Random.Range(1, 24);
+                int num3 = UnityEngine.Random.Range(1, 3);
+                numberNewCard.Add(num1);
+                randomizerNewCard.Add(num2);
+                number2NewCard.Add(num3);
+            }
+
             wannaRandomNumbers = false;
         }
 
@@ -154,67 +165,67 @@ public class ServerManager : MonoBehaviour
         CardBase _newCard = new CardBase(CardType.None);
 
         //Not following Cards has a normal probability to appear
-        if (randomizerNewCard >= 1 && randomizerNewCard <= 4)
+        if (randomizerNewCard[0] >= 1 && randomizerNewCard[0] <= 4)
         {
-            if (randomizerNewCard == 1)
+            if (randomizerNewCard[0] == 1)
                 _newCard.cardType = CardType.NotFollowingRed;
-            else if (randomizerNewCard == 2)
+            else if (randomizerNewCard[0] == 2)
                 _newCard.cardType = CardType.NotFollowingBlue;
-            else if (randomizerNewCard == 3)
+            else if (randomizerNewCard[0] == 3)
                 _newCard.cardType = CardType.NotFollowingGreen;
-            else if (randomizerNewCard == 4)
+            else if (randomizerNewCard[0] == 4)
                 _newCard.cardType = CardType.NotFollowingYellow;
         }
 
         //Sum Basic Cards has a normal probability to appear
-        else if (randomizerNewCard >= 5 && randomizerNewCard <= 8)
+        else if (randomizerNewCard[0] >= 5 && randomizerNewCard[0] <= 8)
         {
-            if (number2NewCard == 1)
-                number2NewCard = 2;
+            if (number2NewCard[0] == 1)
+                number2NewCard[0] = 2;
             else
-                number2NewCard = 4;
+                number2NewCard[0] = 4;
 
-            if (randomizerNewCard == 5)
+            if (randomizerNewCard[0] == 5)
                 _newCard.cardType = CardType.SumRed;
-            else if (randomizerNewCard == 6)
+            else if (randomizerNewCard[0] == 6)
                 _newCard.cardType = CardType.SumBlue;
-            else if (randomizerNewCard == 7)
+            else if (randomizerNewCard[0] == 7)
                 _newCard.cardType = CardType.SumGreen;
-            else if (randomizerNewCard == 8)
+            else if (randomizerNewCard[0] == 8)
                 _newCard.cardType = CardType.SumYellow;
 
-            _newCard.num = number2NewCard;
+            _newCard.num = number2NewCard[0];
         }
 
         //Black Cards has a normal probability to appear
-        else if (randomizerNewCard == 9 || randomizerNewCard == 10)
+        else if (randomizerNewCard[0] == 9 || randomizerNewCard[0] == 10)
         {
             _newCard.cardType = CardType.BlackColorCard;
         }
-        else if (randomizerNewCard == 11)
+        else if (randomizerNewCard[0] == 11)
         {
-            if (number2NewCard == 1)
-                number2NewCard = 2;
+            if (number2NewCard[0] == 1)
+                number2NewCard[0] = 2;
             else
-                number2NewCard = 4;
+                number2NewCard[0] = 4;
 
             _newCard.cardType = CardType.BlackSum4Card;
-            _newCard.num = number2NewCard;
+            _newCard.num = number2NewCard[0];
         }
 
         //Basic Cards has a double probability to appear
         else
         {
-            if (randomizerNewCard == 12 || randomizerNewCard == 13 || randomizerNewCard == 14)
+            if (randomizerNewCard[0] == 12 || randomizerNewCard[0] == 13 || randomizerNewCard[0] == 14)
                 _newCard.cardType = CardType.RedCard;
-            else if (randomizerNewCard == 15 || randomizerNewCard == 16 || randomizerNewCard == 17)
+            else if (randomizerNewCard[0] == 15 || randomizerNewCard[0] == 16 || randomizerNewCard[0] == 17)
                 _newCard.cardType = CardType.BlueCard;
-            else if (randomizerNewCard == 18 || randomizerNewCard == 19 || randomizerNewCard == 20)
+            else if (randomizerNewCard[0] == 18 || randomizerNewCard[0] == 19 || randomizerNewCard[0] == 20)
                 _newCard.cardType = CardType.GreenCard;
-            else if (randomizerNewCard == 21 || randomizerNewCard == 22 || randomizerNewCard == 23)
+            else if (randomizerNewCard[0] == 21 || randomizerNewCard[0] == 22 || randomizerNewCard[0] == 23)
                 _newCard.cardType = CardType.YellowCard;
 
-            _newCard.num = numberNewCard;
+            _newCard.num = numberNewCard[0];
         }
 
         Debug.Log("Created a new Card: " + _newCard.cardType.ToString() + " with this number: " + _newCard.num);
@@ -223,6 +234,89 @@ public class ServerManager : MonoBehaviour
         wannaRandomNumbers = true;
         wannaUpdateInfo = true;
     }
+    public void SumToAllPlayerNumberCards(int numberCardsToSum)
+    {
+        int count = 0;
+        for(int i = 0; i < userList.Count; i++)//Do the loop for all players
+        {
+            if (userList[i] == null || userList[i].userName == "") //If he player is null in the list or has been deleted not sum the cards
+                continue;
+
+            for(int j = 0; j < numberCardsToSum; j++) //Do the loop x times to get the new Cards
+            {
+
+                CardBase _newCard = new CardBase(CardType.None);
+
+                //Not following Cards has a normal probability to appear
+                if (randomizerNewCard[count] >= 1 && randomizerNewCard[count] <= 4)
+                {
+                    if (randomizerNewCard[count] == 1)
+                        _newCard.cardType = CardType.NotFollowingRed;
+                    else if (randomizerNewCard[count] == 2)
+                        _newCard.cardType = CardType.NotFollowingBlue;
+                    else if (randomizerNewCard[count] == 3)
+                        _newCard.cardType = CardType.NotFollowingGreen;
+                    else if (randomizerNewCard[count] == 4)
+                        _newCard.cardType = CardType.NotFollowingYellow;
+                }
+
+                //Sum Basic Cards has a normal probability to appear
+                else if (randomizerNewCard[count] >= 5 && randomizerNewCard[count] <= 8)
+                {
+                    if (number2NewCard[count] == 1)
+                        number2NewCard[count] = 2;
+                    else
+                        number2NewCard[count] = 4;
+
+                    if (randomizerNewCard[count] == 5)
+                        _newCard.cardType = CardType.SumRed;
+                    else if (randomizerNewCard[count] == 6)
+                        _newCard.cardType = CardType.SumBlue;
+                    else if (randomizerNewCard[count] == 7)
+                        _newCard.cardType = CardType.SumGreen;
+                    else if (randomizerNewCard[count] == 8)
+                        _newCard.cardType = CardType.SumYellow;
+
+                    _newCard.num = number2NewCard[count];
+                }
+
+                //Black Cards has a normal probability to appear
+                else if (randomizerNewCard[count] == 9 || randomizerNewCard[count] == 10)
+                {
+                    _newCard.cardType = CardType.BlackColorCard;
+                }
+                else if (randomizerNewCard[count] == 11)
+                {
+                    if (number2NewCard[count] == 1)
+                        number2NewCard[count] = 2;
+                    else
+                        number2NewCard[count] = 4;
+
+                    _newCard.cardType = CardType.BlackSum4Card;
+                    _newCard.num = number2NewCard[count];
+                }
+
+                //Basic Cards has a double probability to appear
+                else
+                {
+                    if (randomizerNewCard[count] == 12 || randomizerNewCard[count] == 13 || randomizerNewCard[count] == 14)
+                        _newCard.cardType = CardType.RedCard;
+                    else if (randomizerNewCard[count] == 15 || randomizerNewCard[count] == 16 || randomizerNewCard[count] == 17)
+                        _newCard.cardType = CardType.BlueCard;
+                    else if (randomizerNewCard[count] == 18 || randomizerNewCard[count] == 19 || randomizerNewCard[count] == 20)
+                        _newCard.cardType = CardType.GreenCard;
+                    else if (randomizerNewCard[count] == 21 || randomizerNewCard[count] == 22 || randomizerNewCard[count] == 23)
+                        _newCard.cardType = CardType.YellowCard;
+
+                    _newCard.num = numberNewCard[count];
+                }
+                userList[i].cardList.Add(_newCard); //Add the new Card to the user
+                count++;
+            }
+        }
+        wannaUpdateInfo = true;
+        wannaRandomNumbers = true;
+    }
     #endregion
 
     #region UpdateUI
@@ -230,15 +324,16 @@ public class ServerManager : MonoBehaviour
     {
         for(int i = 0; i < userList.Count; i++)
         {
-            if(userList[i]!=null) playerProperties[i].user = userList[i];
+            if (userList[i] != null) playerProperties[i].user = userList[i];
             if (userList[i].userStatus == UserStatus.Disconnected)
             {
                 PutOneUserDisconnected(i);
-                return;
+                continue;
             }
             playerProperties[i].userName.text = userList[i].userName;
             playerProperties[i].userImage.sprite = spritesPerfil[userList[i].userImage];
             playerProperties[i].numberofCards.text = "Number of cards: " + userList[i].cardList.Count.ToString();
+            playerProperties[i].InstantiateNewCard();
             switch (userList[i].userStatus)
             {
                 case (UserStatus.Connected):
@@ -262,7 +357,6 @@ public class ServerManager : MonoBehaviour
                     playerProperties[i].userStatus.color = Color.green;
                     break;
             }
-            playerProperties[i].InstantiateNewCard();
         }
     }
     public void UpdateGameUI()
@@ -278,6 +372,13 @@ public class ServerManager : MonoBehaviour
         playerProperties[_index].userStatus.text = "DISCONNECTED";
         playerProperties[_index].userStatus.color = Color.red;
         playerProperties[_index].numberofCards.text = "Number of cards: 0";
+
+        for(int i = 0; i < playerProperties[_index].cardsGOList.Count; i++)
+        {
+            Destroy(playerProperties[_index].cardsGOList[i]);
+        }
+        playerProperties[_index].cardsGOList.Clear();
+        playerProperties[_index].user.cardList.Clear();
     }
     #endregion
 
